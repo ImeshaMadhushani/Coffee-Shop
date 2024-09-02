14e2eee966f714c9612969eef431d176e4613c7d
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2024-09-02 11:47:45 +0530

    except cart functionalities

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ const navbar = document.querySelector('.navbar');
 menuToggle.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
+
+
+ document.querySelector('.search-btn').addEventListener('click', function() {
+    let searchTerm = document.querySelector('.search input').value.toLowerCase();
+    let productBoxes = document.querySelectorAll('.products-container .box');
+    
+    let found = false;
+
+    productBoxes.forEach(box => {
+        let productName = box.querySelector('h3').textContent.toLowerCase();
+        if (productName.includes(searchTerm)) {
+            box.style.display = 'block';
+            found = true;
+            
+        } else {
+            box.style.display = 'none';
+        }
+    });
+
+    if (found) {
+        document.querySelector('#products').scrollIntoView({ behavior: 'smooth' });
+    } else {
+        alert("No products match your search.");
+    }
+ });
+
+
+
+ 
